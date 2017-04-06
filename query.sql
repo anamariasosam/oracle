8dfe68e63e800d0cc564aadc4c40c07a8b8f3119
Ana Maria Sosa <anamariasosam@gmail.com>
2017-04-06 16:02:10 -0500

    add second query

--- query.sql
+++ query.sql
@@ -5,3 +5,11 @@ WHERE psventa =
   (SELECT min(`psventa`) FROM salvarez_prueba.TSIM_PROD_X_EMPRESA)
 AND CDPRODUCTO = '01'
 LIMIT 1
+
+SELECT persona_id
+FROM productos
+WHERE psventa = (SELECT min(`psventa`) FROM productos)
+AND NAME = 'Aceituna'
+AND nmcantidad >= 3
+ORDER BY nmcantidad DESC
+LIMIT 1;
