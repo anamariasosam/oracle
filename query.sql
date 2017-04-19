86780185a5c9e6c30703bfc3a8866301a404d6e8
ana <anamariasosam@gmail.com>
2017-04-19 11:55:49 -0500

    add files

--- query.sql
+++ query.sql
@@ -2,14 +2,29 @@
 SELECT CDEMPRESA
 FROM salvarez_prueba.TSIM_PROD_X_EMPRESA
 WHERE psventa =
-  (SELECT min(`psventa`) FROM salvarez_prueba.TSIM_PROD_X_EMPRESA)
+  (
+  SELECT min(psventa)
+  FROM salvarez_prueba.TSIM_PROD_X_EMPRESA
+  AND CDPRODUCTO = '01'
+  WHERE nmcantidad > 3
+  )
 AND CDPRODUCTO = '01'
-LIMIT 1
+AND ROWNUM = 1;
 
-SELECT persona_id
-FROM productos
-WHERE psventa = (SELECT min(`psventa`) FROM productos)
-AND NAME = 'Aceituna'
-AND nmcantidad >= 3
-ORDER BY nmcantidad DESC
-LIMIT 1;
+
+declare
+v_DSERROR varchar2(200);
+v_NMCANTIDAD_DEBITAR salvarez_prueba.TSIM_PROD_X_EMPRESA.NMCANTIDAD%TYPE;
+v_PSVALOR_UNITARIO_VENTA salvarez_prueba.TSIM_PROD_X_EMPRESA.PSVENTA%TYPE;
+BEGIN
+  SP_COMPRAR_PROD_RES
+  (
+  21,
+  01,
+  2,
+  v_DSERROR,
+  v_NMCANTIDAD_DEBITAR,
+  v_PSVALOR_UNITARIO_VENTA
+  );
+END;
+/
